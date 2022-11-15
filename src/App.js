744dc27feb0cd02d8734305716f3d00aa3bd6e23
JENIL SANGANI <jenilsangani2000@gmail.com>
2022-11-15 04:18:22 -0500

    add data on app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import NavBar from './components/NavBar'
-import News from './components/News'
+import "./App.css";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <NavBar/>
-        <News/>
+        <NavBar />
+        <News />
       </div>
-    )
+    );
   }
 }
