1e191daef207865d3ee664582e254eeaafb7d535
JENIL SANGANI <jenilsangani2000@gmail.com>
2022-11-18 05:58:41 -0500

    handleArrow added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <a className="navbar-brand" href="/">
               NewsMonkey
