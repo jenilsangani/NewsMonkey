285dc1d82e168c2b56b5261beb2a0f04559290c7
JENIL SANGANI <jenilsangani2000@gmail.com>
2022-11-23 04:55:36 -0500

    Updates

---
+++ precache-manifest.6b6ecd236307b1d84f9ba79246de816a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3731c350d289b399b93",
+    "url": "/NewsMonkey/static/css/main.235a1fbb.chunk.css"
+  },
+  {
+    "revision": "a3731c350d289b399b93",
+    "url": "/NewsMonkey/static/js/main.a3731c35.chunk.js"
+  },
+  {
+    "revision": "fbee9149cc341e4ff9a0",
+    "url": "/NewsMonkey/static/js/1.fbee9149.chunk.js"
+  },
+  {
+    "revision": "7fbc085470e4c9b007de",
+    "url": "/NewsMonkey/static/js/2.7fbc0854.chunk.js"
+  },
+  {
+    "revision": "831265046452af99c0b1",
+    "url": "/NewsMonkey/static/js/runtime~main.83126504.js"
+  },
+  {
+    "revision": "ff60cbbb2cac15e6efaf94fcdc57b14d",
+    "url": "/NewsMonkey/index.html"
+  }
+];
