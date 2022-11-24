0fcc46519410a4e2bf41798a1ecc023d458401d5
JENIL SANGANI <jenilsangani2000@gmail.com>
2022-11-23 23:58:08 -0500

    change calss

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ export default class App extends Component {
         <Router>
           <NavBar />
           <Routes>
-            <Route path="/home" element={<News key="home" pageSize={8} country="in" category="business"/>}></Route>
-            <Route path="/business" element={<News key="business" pageSize={8} country="in" category="business"/>}></Route>
-            <Route path="/entertainment" element={<News key="entertainment" pageSize={8} country="in" category="entertainment"/>}></Route>
-            <Route path="/general" element={<News key="general" pageSize={8} country="in" category="general"/>}></Route>
-            <Route path="/health" element={<News key="health" pageSize={8} country="in" category="health"/>}></Route>
-            <Route path="/science" element={<News key="science" pageSize={8} country="in" category="science"/>}></Route>
-            <Route path="/sports" element={<News key="sports" pageSize={8} country="in" category="sports"/>}></Route>
-            <Route path="/technology" element={<News key="technology" pageSize={8} country="in" category="technology"/>}></Route>
+            <Route path="/home" element={<News key="home" pageSize={10} country="in" category="business"/>}></Route>
+            <Route path="/business" element={<News key="business" pageSize={10} country="in" category="business"/>}></Route>
+            <Route path="/entertainment" element={<News key="entertainment" pageSize={10} country="in" category="entertainment"/>}></Route>
+            <Route path="/general" element={<News key="general" pageSize={10} country="in" category="general"/>}></Route>
+            <Route path="/health" element={<News key="health" pageSize={10} country="in" category="health"/>}></Route>
+            <Route path="/science" element={<News key="science" pageSize={10} country="in" category="science"/>}></Route>
+            <Route path="/sports" element={<News key="sports" pageSize={10} country="in" category="sports"/>}></Route>
+            <Route path="/technology" element={<News key="technology" pageSize={10} country="in" category="technology"/>}></Route>
           </Routes>
         </Router>
         <Copyright/>
